1175c4ac99a8e72e755862f0aaef827c7f2b5ab5
minuum <minwool0357@gmail.com>
2025-05-14 21:54:04 +0900

    Remove obsolete Jupyter notebooks and main experiment runner script to clean up the project

--- main_experiment_runner.py
+++
@@ -1,6 +0,0 @@
-def run_faiss_rag_experiment():
-    """FAISS 기반 RAG 실험 실행 함수"""
-    # 0. 설정
-    qa_dataset_filepath = 'qa_dataset.json' 
-    faiss_cache_dir = "../cached_vectors/balanced_json" # 경로 수정
-    output_dir = "rag_faiss_experiment_results_py" 
